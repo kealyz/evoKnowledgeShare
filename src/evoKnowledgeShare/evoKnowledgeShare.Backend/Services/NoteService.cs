361993e009ccc5856a05c58124cc688a5418c933
Vadászi Dávid <vadaszidavid2@gmail.com>
2022-10-21 16:52:59 +0200

    2022. 10. 21. meeting

---
+++ src/evoKnowledgeShare/evoKnowledgeShare.Backend/Services/NoteService.cs
@@ -0,0 +1,19 @@
+﻿using evoKnowledgeShare.Backend.Interfaces;
+using evoKnowledgeShare.Backend.Models;
+
+namespace evoKnowledgeShare.Backend.Services
+{
+    public class NoteService
+    {
+        private readonly IRepository<Note> myRepository;
+
+        public NoteService(IRepository<Note> repository)
+        {
+            myRepository = repository;
+        }
+
+        public IEnumerable<Note> Get() => myRepository.GetAll();
+
+
+    }
+}
