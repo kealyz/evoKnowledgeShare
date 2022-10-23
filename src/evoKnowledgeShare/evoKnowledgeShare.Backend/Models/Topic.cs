12eb7d47cbc1e466e53debd3bf02db70eefac858
Orosz Peter <oroszpeter97@gmail.com>
2022-10-23 19:14:31 +0200

    Topic és TopicServices kidolgozása
    Megcsináltam a Topic és TopicServices classt és megcsináltam a metódusokat.

---
+++ src/evoKnowledgeShare/evoKnowledgeShare.Backend/Models/Topic.cs
@@ -0,0 +1,11 @@
+﻿namespace evoKnowledgeShare.Backend.Models {
+    public class Topic {
+        public Guid TopicID { get; set; }
+        public string Title { get; set; }
+
+        public Topic(Guid TopicID, string Title) {
+            this.TopicID = TopicID;
+            this.Title = Title;
+        }
+    }
+}
---
+++ src/evoKnowledgeShare/evoKnowledgeShare.Backend/Services/TopicService.cs
@@ -0,0 +1,52 @@
+﻿using evoKnowledgeShare.Backend.Interfaces;
+using evoKnowledgeShare.Backend.Models;
+
+namespace evoKnowledgeShare.Backend.Services {
+    public class TopicService {
+        private readonly IRepository<Topic> myRepository;
+
+        public TopicService(IRepository<Topic> repository) {
+            myRepository = repository;
+        }
+
+        public void Add(Topic topic) {
+            myRepository.Add(topic);
+        }
+
+        public async Task AddAsync(Topic topic) {
+            await myRepository.AddAsync(topic);
+        }
+
+        public void AddRange(IEnumerable<Topic> range) {
+            myRepository.AddRange(range);
+        }
+
+        public IEnumerable<Topic> GetAll() => myRepository.GetAll();
+
+        public async Task<IEnumerable<Topic>> GetAllAsync() {
+            return await myRepository.GetAllAsync();
+        }
+
+        public Topic GetById(int id) {
+            return myRepository.GetById(id);
+        }
+
+        public IEnumerable<Topic> GetRangeById(IEnumerable<int> ids) {
+            return myRepository.GetRangeById(ids);
+        }
+
+        public void Remove(Topic entity) => myRepository.Remove(entity);
+
+        public void RemoveById(int id) => myRepository.RemoveById(id);
+
+        public void RemoveRange(IEnumerable<Topic> entities) => myRepository.RemoveRange(entities);
+
+        public void RemoveRangeById(IEnumerable<int> ids) => myRepository.RemoveRangeById(ids);
+
+        public Task SaveChangesAsync(CancellationToken cancellationToken = default) => myRepository.SaveChangesAsync(cancellationToken);
+
+        public void Update(Topic entity) => myRepository.Update(entity);
+
+        public void UpdateRange(IEnumerable<Topic> entitites) => myRepository.UpdateRange(entitites);
+    }
+}
