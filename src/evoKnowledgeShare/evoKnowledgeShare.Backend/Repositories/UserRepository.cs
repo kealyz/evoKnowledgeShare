7c3482ae9e51d83d57dab1cf55dbce8b82313aab
SzAndras1 <szbandras2@gmail.com>
2022-11-04 16:04:09 +0100

    Update UserRepository.cs

--- src/evoKnowledgeShare/evoKnowledgeShare.Backend/Repositories/UserRepository.cs
+++ src/evoKnowledgeShare/evoKnowledgeShare.Backend/Repositories/UserRepository.cs
@@ -15,10 +15,7 @@ namespace evoKnowledgeShare.Backend.Repositories
 
         public override void AddRange(IEnumerable<User> entities) => myDbContext.Users.AddRange(entities);
 
-        public override Task AddRangeAsync(IEnumerable<User> entities)
-        {
-            throw new NotImplementedException();
-        }
+        public override Task AddRangeAsync(IEnumerable<User> entities) => throw new NotImplementedException();
 
         public override IEnumerable<User> GetAll() => myDbContext.Users;
 
@@ -26,66 +23,33 @@ namespace evoKnowledgeShare.Backend.Repositories
 
         public override User GetById(int id) => myDbContext.Users.First(x => x.Id == id);
 
-        public override Task<User?> GetByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public override Task<User?> GetByIdAsync(int id) => throw new NotImplementedException();
 
-        public override IEnumerable<User> GetRangeById(IEnumerable<int> ids)
-        {
-            throw new NotImplementedException();
-        }
+        public override IEnumerable<User> GetRangeById(IEnumerable<int> ids) => throw new NotImplementedException();
 
         public override void Remove(User entity) => myDbContext.Users.Remove(entity);
 
-        public override Task RemoveAsync(User entity)
-        {
-            throw new NotImplementedException();
-        }
+        public override Task RemoveAsync(User entity) => throw new NotImplementedException();
         public override void RemoveById(int id) => myDbContext.Users.Remove(myDbContext.Users.First(x => x.Id == id));
-        
-        public override Task<IEnumerable<User>> GetRangeByIdAsync(IEnumerable<int> ids)
-        {
-            throw new NotImplementedException();
-        }
 
-        public override Task RemoveByIdAsync(int id)
-        {
-            throw new NotImplementedException();
-        }
+        public override Task<IEnumerable<User>> GetRangeByIdAsync(IEnumerable<int> ids) => throw new NotImplementedException();
 
-        public override void RemoveRange(IEnumerable<User> entities)
-        {
-            throw new NotImplementedException();
-        }
+        public override Task RemoveByIdAsync(int id) => throw new NotImplementedException();
 
-        public override Task RemoveRangeAsync(IEnumerable<User> entities)
-        {
-            throw new NotImplementedException();
-        }
+        public override void RemoveRange(IEnumerable<User> entities) => throw new NotImplementedException();
 
-        public override void RemoveRangeById(IEnumerable<int> ids)
-        {
-            throw new NotImplementedException();
-        }
+        public override Task RemoveRangeAsync(IEnumerable<User> entities) => throw new NotImplementedException();
 
-        public override Task RemoveRangeByIdAsync(IEnumerable<int> ids)
-        {
-            throw new NotImplementedException();
-        }
+        public override void RemoveRangeById(IEnumerable<int> ids) => throw new NotImplementedException();
+
+        public override Task RemoveRangeByIdAsync(IEnumerable<int> ids) => throw new NotImplementedException();
 
         public override void Update(User entity) => myDbContext.Users.Update(entity);
 
-        public override Task UpdateAsync(User entity)
-        {
-            throw new NotImplementedException();
-        }
+        public override Task UpdateAsync(User entity) => throw new NotImplementedException();
 
         public override void UpdateRange(IEnumerable<User> entitites) => myDbContext.Users.UpdateRange(entitites);
-        
-        public override Task UpdateRangeAsync(IEnumerable<User> entitites)
-        {
-            throw new NotImplementedException();
-        }
+
+        public override Task UpdateRangeAsync(IEnumerable<User> entitites) => throw new NotImplementedException();
     }
 }
