8f8748ac56b4b723bb9711663d591e48b05213a8
kealyz <65422949+kealyz@users.noreply.github.com>
2022-12-19 23:05:39 +0100

    Refactored TopicController.

--- src/evoKnowledgeShare/evoKnowledgeShare.Backend/Controllers/TopicController.cs
+++ src/evoKnowledgeShare/evoKnowledgeShare.Backend/Controllers/TopicController.cs
@@ -4,30 +4,50 @@ using evoKnowledgeShare.Backend.Services;
 using Microsoft.AspNetCore.Mvc;
 using System.Net.Mime;
 
-namespace evoKnowledgeShare.Backend.Controllers {
+namespace evoKnowledgeShare.Backend.Controllers
+{
     [Route("api/[controller]")]
     [ApiController]
-    public class TopicController : ControllerBase {
+    public class TopicController : ControllerBase
+    {
         private TopicService myTopicService;
 
-        public TopicController(TopicService myTopicService) {
+        public TopicController(TopicService myTopicService)
+        {
             this.myTopicService = myTopicService;
         }
 
-        [HttpGet("Topics")]
-        public IActionResult GetTopics() {
-            IEnumerable<Topic> result = myTopicService.GetAll();
-            return result.Any() ? Ok(result) : Problem(statusCode: StatusCodes.Status404NotFound, title: "No topics found.");
+        [HttpGet("All")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetTopics()
+        {
+            try
+            {
+                return Ok(myTopicService.GetAll());
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
 
-        [HttpGet("TopicID/{id}")]
-        public IActionResult GetTopicsById(Guid id) {
-            try {
+        [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public IActionResult GetTopicById(Guid id)
+        {
+            try
+            {
                 return Ok(myTopicService.GetById(id));
-            } catch (ArgumentNullException) {
-                throw;
-            } catch (InvalidOperationException) {
-                throw;
+            }
+            catch (ArgumentNullException)
+            {
+                return UnprocessableEntity();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
             }
         }
 
@@ -35,23 +55,67 @@ namespace evoKnowledgeShare.Backend.Controllers {
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status201Created)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
-        public async Task<IActionResult> CreateTopicAsync([FromBody] TopicDTO topicDTO) {
-            Topic? result = await myTopicService.AddAsync(topicDTO);
-            return result is not null ? Created(nameof(CreateTopicAsync), result) : BadRequest("Topic cannot be added.");
+        [ProducesResponseType(StatusCodes.Status409Conflict)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public async Task<IActionResult> CreateTopicAsync([FromBody] TopicDTO topicDTO)
+        {
+            try
+            {
+                return Created(nameof(CreateTopicAsync), await myTopicService.AddAsync(topicDTO));
+            }
+            catch (ArgumentNullException)
+            {
+                return UnprocessableEntity();
+            }
+            catch (OperationCanceledException)
+            {
+                return Conflict();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
         }
 
-        [HttpDelete("Delete")]
+        [HttpPost("Update")]
+        [Consumes(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status422UnprocessableEntity)]
+        public IActionResult UpdateTopic([FromBody] Topic topic)
+        {
+            try
+            {
+                return Ok(myTopicService.Update(topic));
+            }
+            catch (ArgumentNullException)
+            {
+                return UnprocessableEntity();
+            }
+            catch (Exception)
+            {
+                return BadRequest();
+            }
+        }
+
+        [HttpDelete("Delete/{id}")]
         [Consumes(MediaTypeNames.Application.Json)]
         [ProducesResponseType(StatusCodes.Status204NoContent)]
         [ProducesResponseType(StatusCodes.Status400BadRequest)]
         [ProducesResponseType(StatusCodes.Status404NotFound)]
-        public IActionResult DeleteTopic(Guid id) {
-            try {
+        public IActionResult DeleteTopic(Guid id)
+        {
+            try
+            {
                 myTopicService.RemoveById(id);
                 return NoContent();
-            } catch (KeyNotFoundException) {
+            }
+            catch (KeyNotFoundException)
+            {
                 return NotFound();
-            } catch (Exception) {
+            }
+            catch (Exception)
+            {
                 return BadRequest();
             }
         }
