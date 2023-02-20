3b393fa49557924d8c3c7c8d5b0000a3ed3ad8d5
Baumel Márton <marcibaumel@gmail.com>
2023-02-20 15:07:39 +0100

    Simple filtering, card animation, small bug fixes

--- evoKnowledgeShareFrontend/src/pages/Home.tsx
+++ evoKnowledgeShareFrontend/src/pages/Home.tsx
@@ -23,33 +23,49 @@ export const Home = () => {
         <p>If you are not familiar with markdown language please check this sources:</p>
         <div>
           <Card onClick={() => window.open("https://www.markdownguide.org/", "_blank")} className={classes.cardItem} style={{ width: 'auto' }}>
-            <Card.Body>
-              <Card.Title>Markdown Guide</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Click on the card</Card.Subtitle>
-              <Card.Text>
-                The Markdown Guide is a free and open-source reference guide that explains how to use Markdown, the simple and easy-to-use markup language you can use to format virtually any document.
-              </Card.Text>
-            </Card.Body>
+            <motion.div
+              whileHover={{
+                scale: 0.98,
+                transition: { duration: 0.3 },
+              }}>
+              <Card.Body>
+                <Card.Title>Markdown Guide</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Click on the card</Card.Subtitle>
+                <Card.Text>
+                  The Markdown Guide is a free and open-source reference guide that explains how to use Markdown, the simple and easy-to-use markup language you can use to format virtually any document.
+                </Card.Text>
+              </Card.Body>
+            </motion.div>
           </Card>
 
           <Card onClick={() => window.open("https://daringfireball.net/projects/markdown/syntax", "_blank")} className={classes.cardItem} style={{ width: 'auto' }}>
-            <Card.Body>
-              <Card.Title>Markdown: Syntax</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Click on the card</Card.Subtitle>
-              <Card.Text>
-                Markdown is intended to be as easy-to-read and easy-to-write as is feasible.
-              </Card.Text>
-            </Card.Body>
+            <motion.div whileHover={{
+              scale: 0.98,
+              transition: { duration: 0.3 },
+            }}>
+              <Card.Body>
+                <Card.Title>Markdown: Syntax</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Click on the card</Card.Subtitle>
+                <Card.Text>
+                  Markdown is intended to be as easy-to-read and easy-to-write as is feasible.
+                </Card.Text>
+              </Card.Body>
+            </motion.div>
           </Card>
 
           <Card onClick={() => window.open("https://github.com/adam-p/markdown-here/wiki/Markdown-Cheatsheet", "_blank")} className={classes.cardItem} style={{ width: 'auto' }}>
-            <Card.Body>
-              <Card.Title>Markdown Cheatsheet</Card.Title>
-              <Card.Subtitle className="mb-2 text-muted">Click on the card</Card.Subtitle>
-              <Card.Text>
-                This is intended as a quick reference and showcase. For more complete info, see John Gruber's original spec and the Github-flavored Markdown info page.
-              </Card.Text>
-            </Card.Body>
+            <motion.div whileHover={{
+              scale: 0.98,
+              transition: { duration: 0.3 },
+            }}>
+              <Card.Body>
+                <Card.Title>Markdown Cheatsheet</Card.Title>
+                <Card.Subtitle className="mb-2 text-muted">Click on the card</Card.Subtitle>
+                <Card.Text>
+                  This is intended as a quick reference and showcase. For more complete info, see John Gruber's original spec and the Github-flavored Markdown info page.
+                </Card.Text>
+              </Card.Body>
+            </motion.div>
           </Card>
 
         </div>
--- evoKnowledgeShareFrontend/src/pages/Notes.tsx
+++ evoKnowledgeShareFrontend/src/pages/Notes.tsx
@@ -1,6 +1,6 @@
 import { motion } from 'framer-motion';
 import { useEffect, useState } from 'react'
-import { Button, Card } from 'react-bootstrap';
+import { Button, Card, Form } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
 import IDocument from '../interfaces/IDocument';
 
@@ -18,6 +18,7 @@ const containerVariant = {
 
 export const Notes = () => {
   const [notes, setNotes] = useState<IDocument[]>([])
+  const [filterValue, setFilterValue] = useState<string>('');
   const navigate = useNavigate();
 
   const fetchNotes = async () => {
@@ -46,11 +47,11 @@ export const Notes = () => {
         initial="hidden"
         animate="visible">
         <h1 className='mb-4'>Notes</h1>
-
+        <Form.Control type="text" placeholder="Search for title" value={filterValue} onChange={(e) => setFilterValue(e.target.value)} />
         <motion.div variants={containerVariant}
           initial="hidden"
           animate="visible"></motion.div>
-        {notes.map((entry) => (
+        {notes && notes.filter(e => e.title.toLowerCase().includes(filterValue.toLowerCase())).map(entry  => (
           <Card className='m-4'>
             <Card.Header as="h6">{entry.createdAt.slice(0, 10)}</Card.Header>
             <Card.Body>
